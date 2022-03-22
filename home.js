90f00c0ab386af9db38b6fe5013803f1df823832
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 15:14:39 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 function Home(){
   return (
     <Card
-      txtcolor="green"
+      txtcolor="black"
       header="BadBank Home"
       title="Welcome to the Badbank"
       text="Use the navigation bar to access banking tools."
