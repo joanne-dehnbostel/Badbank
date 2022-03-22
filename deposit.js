0e022b107e1b6fa89cce2073b0439b7fa18dba9e
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 15:19:17 -0500

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -3,52 +3,52 @@
    // <h1>Deposit</h1>
  // )
 //}
-function Deposit(){
-  const [show, setShow]         = React.useState(true);
-  const [status, setStatus]     = React.useState('');
-  const [depositAmount, setDepositAmount]         = React.useState('');
-  const ctx = React.useContext(UserContext);  
+//function Deposit(){
+ // const [show, setShow]         = React.useState(true);
+  //const [status, setStatus]     = React.useState('');
+  //const [depositAmount, setDepositAmount]         = React.useState('');
+  //const ctx = React.useContext(UserContext);  
 
-  function validate(field, label){
-      if (!field) {
-        setStatus('Error: ' + label);
-        setTimeout(() => setStatus(''),3000);
-        return false;
-      }
-      return true;
-  }
+  //function validate(field, label){
+      //if (!field) {
+        //setStatus('Error: ' + label);
+        //setTimeout(() => setStatus(''),3000);
+        //return false;
+      //}
+      //return true;
+  //}
 
-  function handleCreate(){
-    console.log(name,email,depositAmount);
-    if (!validate(depositAmount,     'name'))     return;
-    ctx.users.push({name,email,password,balance:100});
-    setShow(false);
-  }    
+  //function handleCreate(){
+    //console.log(depositAmount);
+    //if (!validate(depositAmount,     'name'))     return;
+    //ctx.users.push({depositAmount,balance:100});
+    //setShow(false);
+  //}    
 
-  function clearForm(){
-    setDepositAmount('');
-    setShow(true);
-  }
+  //function clearForm(){
+    //setDepositAmount('');
+    //setShow(true);
+ // }
 
-  return (
-    <Card
-      bgcolor="primary"
-      header="Deposit"
-      status={status}
-      body={show ? (  
-              <>
-              depositAmount<br/>
-              <input type="input" className="form-control" id="depositAmount" placeholder="Enter deposit amount" value={depositAmount} onChange={e => setDepositAmount(e.currentTarget.value)} /><br/>
+ // return (
+    //<Card
+      //bgcolor="primary"
+     // header="Deposit"
+     // status={status}
+     // body={show ? (  
+              //<>
+              //depositAmount<br/>
+              //<input type="input" className="form-control" id="depositAmount" placeholder="Enter deposit amount" value={depositAmount} onChange={e => setDepositAmount(e.currentTarget.value)} /><br/>
               
               
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Deposit Funds</button>
-              </>
-            ):(
-              <>
-              <h5>Success</h5>
-              <button type="submit" className="btn btn-light" onClick={clearForm}>Make another deposit</button>
-              </>
-            )}
-    />
-  )
-}
+             // <button type="submit" className="btn btn-light" onClick={handleCreate}>Deposit Funds</button>
+              //</>
+           // ):(
+             // <>
+              //<h5>Success</h5>
+              //<button type="submit" className="btn btn-light" onClick={clearForm}>Make another deposit</button>
+              //</>
+           // )}
+   //  />
+ // )
+//}
