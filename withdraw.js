870baa77b7f1147df37f5dc9d37e651175837f6c
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 18:22:29 -0500

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -1,5 +1,74 @@
 function Withdraw(){
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState('');
+  const [name, setName]         = React.useState('');
+  const [email, setEmail]       = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [withdrawal, setWithdrawal] = React.useState('');
+  const ctx = React.useContext(UserContext);  
+
+  function validate(field, label){
+      if (!field) {
+        setStatus('Error: ' + label);
+        setTimeout(() => setStatus(''),3000);
+        return false;
+      }
+      return true;
+  }
+
+  function handleCreate(){
+    console.log(name,email,password,deposit);
+    if (!validate(name,     'name'))     return;
+    if (!validate(email,    'email'))    return;
+    if (!validate(password, 'password')) return;
+    if (!validate(withdrawal, 'withdrawal')) return;
+    ctx.users.push({name,email,password,deposit,withdrawal,balance:100});
+    setShow(false);
+  }    
+
+  function clearForm(){
+    setName('');
+    setEmail('');
+    setPassword('');
+    setWithdrawal('');
+    setShow(true);
+  }
+
   return (
-    <h1>Withdraw</h1>
+    <Card
+      bgcolor="primary"
+      header="Create Withdrawal"
+      status={status}
+      body={show ? (  
+              <>
+              Name<br/>
+              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+              Email address<br/>
+              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+              Password<br/>
+              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+                Deposit<br/>
+                <input type="withdrawal" className="form-control" id="withdrawal" placeholder="Enter withdrawal amount" value={deposit} onChange={e => setWithdrawal(e.currentTarget.value)}/><br/>
+                <button type="submit" className="btn btn-light" onClick={handleCreate}>Withdraw Funds</button>
+              </>
+            ):(
+              <>
+              <h5>Success</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another withdrawal</button>
+              </>
+            )}
+    />
   )
 }
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
