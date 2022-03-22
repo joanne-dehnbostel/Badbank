7ecabe5230f450eaa1a9f5eefcb9282edfd40ef0
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 13:12:52 -0500

    Update home.js

--- home.js
+++ home.js
@@ -3,8 +3,8 @@ function Home(){
     <Card
       txtcolor="black"
       header="BadBank Landing Module"
-      title="Welcome to the bank"
-      text="You can move around using the navigation bar."
+      title="Welcome to the Badbank"
+      text="Use the navigation bar to access banking tools."
       body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
     />    
   );  
