ff75a6d3422e37eac150183362f62a4b532ae5d9
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 15:24:56 -0500

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -5,13 +5,5 @@ function AllData(){
     <h5>All Data in Store</h5>
     {JSON.stringify(ctx)}<br/>
     </>
-<Card
-      txtcolor="green"
-      header="All Data"
-      title="All User Data is Shown Here"
-{JSON.stringify(ctx)}
-      text="Use the navigation bar to access banking tools."
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-    />  
   );
 }
