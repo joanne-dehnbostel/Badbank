2ee358bb80f222df3f45d3026eb6107b27d88d61
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 16:55:39 -0500

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -28,25 +28,27 @@ function validate(field, label){
     setShow(true);
   }
 
- return (
+  return (
     <Card
       bgcolor="primary"
-     header="Deposit"
-     status={status}
-     body={show ? (  
+      header="Deposit"
+      status={status}
+      body={show ? (  
               <>
-              deposit<br/>
-              <input type="input" className="form-control" id="deposit" placeholder="Enter deposit amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)} /><br/>
-              
-              
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Deposit Funds</button>
+              Name<br/>
+              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+              Email address<br/>
+              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+              Password<br/>
+              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
               </>
-           ):(
+            ):(
               <>
               <h5>Success</h5>
-              <button type="submit" className="btn btn-light" onClick={clearForm}>Make another deposit</button>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
               </>
             )}
-     />
+    />
   )
 }
