5fc1be52fd23b6d49c7cfaafd0a77cb5a3394dfd
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 17:04:18 -0500

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -35,18 +35,15 @@ function validate(field, label){
       status={status}
       body={show ? (  
               <>
-              Name<br/>
-              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
-              Email address<br/>
-              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
-              Password<br/>
-              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              Deposit<br/>
+              <input type="input" className="form-control" id="deposit" placeholder="Enter deposit" value={name} onChange={e => setDeposit(e.currentTarget.value)} /><br/>
+              
+              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Deposit</button>
               </>
             ):(
               <>
               <h5>Success</h5>
-              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another deposit</button>
               </>
             )}
     />
