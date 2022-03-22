492b87875fb6f3344463ab970ee582eb7921f45a
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-22 17:10:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function Spa() {
   return (
     <HashRouter>
       <NavBar/>
-      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+      <UserContext.Provider value={{users:[{name:'joanne',email:'joanne.dehnbostel@gmail.com',password:'secret',deposit:0,balance:100}]}}>
         <div className="container" style={{padding: "20px"}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
