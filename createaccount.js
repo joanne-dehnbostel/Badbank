076d2bf00ed4bdef870f28e565f45b67829faf4d
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-04-10 14:43:23 -0500

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -20,7 +20,7 @@ function CreateAccount(){
     if (!validate(name,     'name'))     return;
     if (!validate(email,    'email'))    return;
     if (!validate(password, 'password')) return;
-    ctx.users.push({name,email,password,deposit:0,withdrawal:0,balance:100});
+    ctx.users.push({name,email,password,deposit:0,withdrawal:0,balance:0});
     setShow(false);
   }    
 
