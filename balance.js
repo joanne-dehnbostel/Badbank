857b85c971f95ebeb1cfa6398356248a512d3154
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-08-31 01:27:09 -0500

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,14 +1,5 @@
 function Balance(){
   return (
     <h1>Balance</h1>
-    function AllData(){
-  const ctx = React.useContext(UserContext);
-  return (
-    <>
-    <h5>All Data in Bank</h5>
-    {JSON.stringify(ctx)}<br/>
-    </>
-  );
-}
   )
 }
