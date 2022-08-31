94f9c63d33714b3c3c9dd25145502d756b9bb962
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-08-31 00:42:36 -0500

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -6,30 +6,4 @@ function AllData(){
     {JSON.stringify(ctx)}<br/>
     </>
   );
-return (
-    <Card
-      bgcolor="primary"
-      header="Create Deposit"
-      status={status}
-      body={show ? (  
-              <>
-              Name<br/>
-              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
-              Email address<br/>
-              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
-              Password<br/>
-              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-                Deposit<br/>
-                <input type="deposit" className="form-control" id="deposit" placeholder="Enter deposit amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}/><br/>
-                <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Deposit</button>
-              </>
-            ):(
-              <>
-              <h5>Success</h5>
-              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another deposit</button>
-              </>
-            )}
-    />
-  )
-}
-}
+
